8ddede8d8eac88e694f4dd2f5e4098a5e2648d0b
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-06-23 16:43:35 +0200

    feature: add gradient effect below cursor

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import CustomContainer from './atomics/custom-container.vue'
+import GradientBelowCursor from './atomics/gradient-below-cursor.vue'
 </script>
 
 <template>
+  <GradientBelowCursor></GradientBelowCursor>
   <CustomContainer>
     <header>
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
---
+++ src/atomics/gradient-below-cursor.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const mouseX = ref(0)
+const mouseY = ref(0)
+
+const updateMousePosition = (event: MouseEvent) => {
+  mouseX.value = event.clientX
+  mouseY.value = event.clientY
+}
+
+onMounted(() => {
+  window.addEventListener('mousemove', updateMousePosition)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', updateMousePosition)
+})
+</script>
+
+<template>
+  <div
+    class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+    :style="{
+      background: `radial-gradient(600px at ${mouseX}px ${mouseY}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+    }"
+  ></div>
+</template>
