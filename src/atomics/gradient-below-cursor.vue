c8bde71bf46d86bbfbf67e65f09bb9f3206219a5
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-06-23 17:43:39 +0200

    style: update light mode gradient color for improved visibility

--- src/atomics/gradient-below-cursor.vue
+++ src/atomics/gradient-below-cursor.vue
@@ -22,7 +22,7 @@ onUnmounted(() => {
   <div
     class="pointer-events-none fixed inset-0 z-30 bg-(image:--cursor-light) transition duration-300 lg:absolute dark:bg-(image:--cursor-dark)"
     :style="{
-      '--cursor-light': `radial-gradient(600px at ${mouseX}px ${mouseY}px, rgba(255, 215, 0, 0.06), transparent 80%)`,
+      '--cursor-light': `radial-gradient(600px at ${mouseX}px ${mouseY}px, rgba(204, 196, 102, 0.15), transparent 80%)`,
       '--cursor-dark': `radial-gradient(600px at ${mouseX}px ${mouseY}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
     }"
   ></div>
