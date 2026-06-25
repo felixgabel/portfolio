a0f92396dcfc715b491262bfddba07b6a358ed04
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-06-25 12:52:31 +0200

    refactor: clean up App.vue and remove unused components and assets

--- src/App.vue
+++ src/App.vue
@@ -29,85 +29,20 @@ import BodyContainer from './components/BodyContainer.vue'
     random-factor="0.02"
     dot-stretch="false"
   > -->
-  <BodyContainer class="overflow-auto">
-    <!-- overflow-auto is only necesarry when I use dot-wave -->
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
 
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-    <RouterView />
-  </BodyContainer>
+  <main>
+    <BodyContainer class="overflow-auto">
+      <!-- overflow-auto is only necesarry when I use dot-wave -->
+      <RouterView />
+    </BodyContainer>
+  </main>
+
   <!-- </dot-wave> -->
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
