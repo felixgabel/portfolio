8b59e6564afae78c18addcca7812cfbc23ab4032
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-07-23 20:37:58 +0200

    refactor: remove unused DotWave.js and dotwave-element.js files; update App.vue imports and template

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,21 @@ import { RouterView } from 'vue-router'
 import BodyContainer from './components/BodyContainer.vue'
 import MainHeader from './components/MainHeader.vue'
 
-// import CursorGradient from './components/CursorGradient.vue'
-// import BranchingBackground from './components/background-canvas/BranchingBackground.vue'
+import CursorGradient from './components/CursorGradient.vue'
+import BranchingBackground from './components/background-canvas/BranchingBackground.vue'
 import DottedBackground from './components/background-canvas/DottedBackground.vue'
 import BackdropFilter from './components/background-canvas/BackdropFilter.vue'
-// import DottedGravityBackground from './components/background-canvas/RepulsiveDottedBackground.vue'
-
-// // if I want to use a toggle, use useDark instead. It pairs nicely with tailwind as well.
-// import { usePreferredDark } from '@vueuse/core'
-// const isPreferredDark = usePreferredDark()
+import DottedGravityBackground from './components/background-canvas/DottedGravityBackground.vue'
 </script>
 
 <template>
   <!-- <CursorGradient></CursorGradient> -->
   <!-- <BranchingBackground></BranchingBackground> -->
-  <DottedBackground></DottedBackground>
-  <BackdropFilter></BackdropFilter>
-  <!-- <DottedGravityBackground></DottedGravityBackground> -->
-  <!-- <dot-wave
-    class="flex h-dvh!"
-    num-dots="800"
-    :dot-color="isPreferredDark ? '#333333' : '#cccccc'"
-    background-color="transparent"
-    dot-min-size="1.4"
-    dot-max-size="1.4"
-    dot-min-opacity="0.1"
-    dot-max-opacity="1"
-    random-factor="0.02"
-    dot-stretch="false"
-  > -->
+  <!-- <DottedBackground></DottedBackground> -->
+  <!-- <BackdropFilter></BackdropFilter> -->
+  <DottedGravityBackground></DottedGravityBackground>
 
   <BodyContainer>
-    <!-- overflow-auto on BodyContainer is only necessary when I use dot-wave -->
     <div class="lg:flex lg:justify-between lg:gap-4">
       <MainHeader></MainHeader>
 
@@ -44,5 +27,4 @@ import BackdropFilter from './components/background-canvas/BackdropFilter.vue'
     </div>
   </BodyContainer>
 
-  <!-- </dot-wave> -->
 </template>
