96fe2e285443f361a43743494f62edc51a7ee43d
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-07-24 12:45:49 +0200

    refactor: remove unused BranchingBackground component and related imports from App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,12 @@ import { RouterView } from 'vue-router'
 import BodyContainer from './components/BodyContainer.vue'
 import MainHeader from './components/MainHeader.vue'
 
-import BranchingBackground from './components/background-canvas/BranchingBackground.vue'
 import DottedBackground from './components/background-canvas/DottedBackground.vue'
 import BackdropFilter from './components/background-canvas/BackdropFilter.vue'
 import DottedGravityBackground from './components/background-canvas/DottedGravityBackground.vue'
 </script>
 
 <template>
-  <!-- <BranchingBackground></BranchingBackground> -->
   <!-- <DottedBackground></DottedBackground> -->
   <!-- <BackdropFilter></BackdropFilter> -->
   <DottedGravityBackground></DottedGravityBackground>
