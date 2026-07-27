7294ba81c1ac5c6f550a6c7381b91481c1bbd21b
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-07-27 14:41:02 +0200

    feature: conditionally render MagneticDotGridCanvas based on breakpoints

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import { RouterView } from 'vue-router'
 import BodyContainer from './components/BodyContainer.vue'
 import MainHeader from './components/MainHeader.vue'
 import MagneticDotGridCanvas from './components/MagneticDotGridCanvas.vue'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const showMagneticGrid = breakpoints.greaterOrEqual('lg')
 </script>
 
 <template>
-  <MagneticDotGridCanvas></MagneticDotGridCanvas>
+  <MagneticDotGridCanvas v-if="showMagneticGrid"></MagneticDotGridCanvas>
 
   <BodyContainer>
     <div class="lg:flex lg:justify-between lg:gap-4">
