dd756218cf3c34dbf6443260064aaf7382b4e74b
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-08-18 16:45:19 +0200

    style: make sticky header look good in dark mode

--- src/components/home/sections/SectionHeading.vue
+++ src/components/home/sections/SectionHeading.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useElementBounding } from '@vueuse/core'
+
+const stickyHeader = ref<HTMLElement | null>(null)
+const { top } = useElementBounding(stickyHeader)
+
+const isStuck = computed(() => top.value <= 0)
+</script>
+
 <template>
   <div
-    class="sticky top-0 z-1 -mx-6 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only"
+    ref="stickyHeader"
+    class="sticky top-0 z-1 -mx-6 -mb-15 flex h-15 w-screen items-center bg-linear-to-b from-white to-white/10 to-80% px-6 backdrop-blur transition-shadow md:-mx-12 md:px-12 lg:sr-only dark:from-black dark:to-black/10 dark:shadow-white/12 dark:backdrop-brightness-120"
+    :class="{
+      'shadow-sm': isStuck,
+    }"
   >
     <h2
       class="text-sm font-bold tracking-widest text-neutral-800 uppercase lg:sr-only dark:text-neutral-200"
@@ -8,7 +22,7 @@
       <slot></slot>
     </h2>
   </div>
-  <div class="h-4">
-    <!-- I rather use this solution instead of a simple margin, because otherwise when scrolling the section out of view would cause layout shift and look kinda weird -->
+  <div class="h-19">
+    <!-- this spacer actually just adds 'h-4' but I have to account for the negative margin on the sticky header, which is '-mb-15' -->
   </div>
 </template>
