078322378c92b31b3471e53ca39161d1d8f86f93
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-08-20 13:19:19 +0200

    fix: update dark mode color for SectionDivider component

--- src/components/home/sections/SectionDivider.vue
+++ src/components/home/sections/SectionDivider.vue
@@ -1,6 +1,6 @@
 <template>
   <hr
-    class="h-px border-0 bg-radial from-neutral-200 to-transparent to-85% dark:from-neutral-800"
+    class="h-px border-0 bg-radial from-neutral-200 to-transparent to-85% dark:from-neutral-700"
     aria-hidden="true"
   />
 </template>
