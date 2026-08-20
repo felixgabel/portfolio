bcbcf020e354ee2b1bfff1b144e319b7e9ff53b1
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-08-20 09:26:27 +0200

    feature: add Impressum page and update routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/impressum',
+      name: 'impressum',
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Impressum.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/ImpressumView.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
---
+++ src/views/ImpressumView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="impressum">
+    <h1>Impressum</h1>
+    <h2>Angaben gemäß § 5 DDG</h2>
+    <p>
+      Felix Gabel<br />
+      Kirchstr. 9<br />
+      87642 Halblech<br />
+    </p>
+    <h2>Kontakt</h2>
+    <p>
+      Telefon: +49 176 5575 0285<br />
+      E-Mail: gabelfelix.dev@icloud.com<br />
+    </p>
+    <h2>Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV</h2>
+    <p>
+      Felix Gabel<br />
+      Kirchstr. 9<br />
+      87642 Halblech<br />
+    </p>
+  </div>
+</template>
