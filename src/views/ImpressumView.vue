03e9a59d41bcf48abffe240e9a24312936c9cd04
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-08-20 09:53:25 +0200

    feature: enhance Impressum page layout and structure

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,22 +1,64 @@
 <template>
-  <div class="impressum">
-    <h1>Impressum</h1>
-    <h2>Angaben gemäß § 5 DDG</h2>
-    <p>
-      Felix Gabel<br />
-      Kirchstr. 9<br />
-      87642 Halblech<br />
-    </p>
-    <h2>Kontakt</h2>
-    <p>
-      Telefon: +49 176 5575 0285<br />
-      E-Mail: gabelfelix.dev@icloud.com<br />
-    </p>
-    <h2>Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV</h2>
-    <p>
-      Felix Gabel<br />
-      Kirchstr. 9<br />
-      87642 Halblech<br />
-    </p>
+  <div class="flex flex-col gap-10 pb-16 text-base lg:py-24">
+    <h1 class="text-4xl font-bold tracking-tight text-neutral-800 dark:text-neutral-200">
+      Impressum
+    </h1>
+
+    <section class="flex flex-col gap-2">
+      <h2 class="font-bold tracking-tight text-neutral-700 uppercase dark:text-neutral-300">
+        Angaben gemäß § 5 DDG
+      </h2>
+      <address class="text-sm leading-snug not-italic">
+        <p>Felix Gabel</p>
+        <p>Kirchstr. 9</p>
+        <p>87642 Halblech</p>
+      </address>
+    </section>
+
+    <section class="flex flex-col gap-2">
+      <h2 class="font-bold tracking-tight text-neutral-700 uppercase dark:text-neutral-300">
+        Kontakt
+      </h2>
+      <address class="text-sm leading-snug not-italic">
+        <p>
+          Telefon:
+          <a href="tel:+4917655750285" class="font-medium text-neutral-700 dark:text-neutral-300">
+            +49 176 5575 0285
+          </a>
+        </p>
+        <p>
+          E-Mail:
+          <a
+            href="mailto:gabelfelix.dev@icloud.com"
+            class="font-medium text-neutral-700 dark:text-neutral-300"
+          >
+            gabelfelix.dev@icloud.com
+          </a>
+        </p>
+      </address>
+    </section>
+
+    <section class="flex flex-col gap-2">
+      <h2 class="font-bold tracking-tight text-neutral-700 uppercase dark:text-neutral-300">
+        Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV
+      </h2>
+      <address class="text-sm leading-snug not-italic">
+        <p>Felix Gabel</p>
+        <p>Kirchstr. 9</p>
+        <p>87642 Halblech</p>
+      </address>
+    </section>
+    <hr class="mt-4 border-neutral-200 dark:border-neutral-700" />
+    <footer class="text-sm text-neutral-500">
+      <p>
+        Generiert mit
+        <a
+          href="https://impressumgenerator.com/"
+          class="font-medium text-neutral-600 dark:text-neutral-400"
+        >
+          impressumgenerator.com
+        </a>
+      </p>
+    </footer>
   </div>
 </template>
