a2776544db29f0ff9f2f0657836e00956abc3507
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-08-21 15:39:03 +0200

    chore: update base path in Vite configuration for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.GITHUB_ACTIONS ? '/portfolio/' : '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
